c1866a3ff1e37e04d418967f95a56fcc0fdd8e5c
jameelag1995 <agbariajameel@gmail.com>
2023-11-11 22:11:34 +0200

    search and sort done

--- JS/homepage.js
+++ JS/homepage.js
@@ -1,61 +1,93 @@
-import {usersInfo,flights} from "./localstorage.js";
+import { usersInfo, flights } from "./localstorage.js";
 console.log(usersInfo[0]);
-let currUser = usersInfo.filter(user => user.currentUser === true)[0];
-const username = document.getElementById('username');
-const addFlight = document.getElementById('add-flight');
+let currUser = usersInfo.filter((user) => user.currentUser === true)[0];
+const username = document.getElementById("username");
+const addFlight = document.getElementById("add-flight");
 
-
-if(currUser.isAdmin === true){
-    addFlight.style.display = 'block';
+if (currUser.isAdmin === true) {
+    addFlight.style.display = "block";
     username.innerText = `Admin ${currUser.email}`;
-} else{
-    addFlight.style.display = 'none';
+} else {
+    addFlight.style.display = "none";
     username.innerText = `${currUser.email}`;
 }
 
-const flightsBoard = document.getElementById('flights-board');
+const flightsBoard = document.getElementById("flights-board");
 showTickets(flights);
 
-function showTickets(flightsArr){
-    flightsBoard.innerHTML = '';
-    flightsArr.forEach(flight =>{
-        let ticket = document.createElement('div');
-        ticket.innerHTML = `<div id="from-to">${flight.from} &#8594; ${flight.to}</div>
+function showTickets(flightsArr) {
+    flightsBoard.innerHTML = "";
+    flightsArr.forEach((flight) => {
+        let ticket = document.createElement("div");
+        ticket.innerHTML = `<div id="from-to">${flight.from} &#8594; ${
+            flight.to
+        }</div>
         <div id="departure-return-dates">${flight.dates[0].depart.toDateString()} &#8594; ${flight.dates[1].return.toDateString()}</div>
-        <div id="price">${flight.price}$ <button class="btn add-to-cart">Add to Cart</button><button class="btn edit-price">Edit Price</button></div>`;
-        ticket.classList.add('ticket');
+        <div id="price">${
+            flight.price
+        }$ <button class="btn add-to-cart">Add to Cart</button><button class="btn edit-price">Edit Price</button></div>`;
+        ticket.classList.add("ticket");
         flightsBoard.append(ticket);
     });
-    
 }
 
-
-addFlight.addEventListener('submit', e =>{
+addFlight.addEventListener("submit", (e) => {
     e.preventDefault();
-    const from = document.getElementById('add-flight-from').value;
-    const to = document.getElementById('add-flight-to').value;
-    const price = document.getElementById('add-flight-price').value;
-    const depart = new Date (document.getElementById('add-flight-departure-date').value);
-    const returnDate = new Date (document.getElementById('add-flight-return-date').value);
-    let currFlight = {from,to,price,dates:[{depart},{return:returnDate}]};
+    const from = document.getElementById("add-flight-from").value;
+    const to = document.getElementById("add-flight-to").value;
+    const price = document.getElementById("add-flight-price").value;
+    const depart = new Date(
+        document.getElementById("add-flight-departure-date").value
+    );
+    const returnDate = new Date(
+        document.getElementById("add-flight-return-date").value
+    );
+    let currFlight = {
+        from,
+        to,
+        price,
+        dates: [{ depart }, { return: returnDate }],
+    };
     console.log(currFlight);
     flights.unshift(currFlight);
     showTickets(flights);
 });
 
-const sortBtn = document.getElementById('sort-by-price-btn');
-sortBtn.addEventListener('click', e =>{
-    const sortedFlights = flights.sort((flightA,flightB) =>{
+const sortBtn = document.getElementById("sort-by-price-btn");
+sortBtn.addEventListener("click", (e) => {
+    const sortedFlights = flights.sort((flightA, flightB) => {
         return flightA.price - flightB.price;
     });
+    console.log("sorted flights: ", sortedFlights);
     showTickets(sortedFlights);
 });
 
-const searchFlight = document.getElementById('search-flight');
-searchFlight.addEventListener('submit', e =>{
-    const filteredFlights = flights.filter(flight => {
-        
-    })
-})
+const searchFlight = document.getElementById("search-flight");
+searchFlight.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const from = document.getElementById("flight-from").value.toLowerCase();
+    const to = document.getElementById("flight-to").value.toLowerCase();
+    const price = document.getElementById("flight-price").value;
+    const depart = new Date(document.getElementById("departure-date").value);
+    const returnDate = new Date(document.getElementById("return-date").value);
+    const filteredFlights = flights.filter((flight) => {
+        // console.log(flight.from,from,flight.from.toLowerCase().includes(from));
+        // console.log(flight.to.toLowerCase().includes(to));
+        // console.log(flight.price <= price);
+        // console.log(flight.depart >= depart);
+        // console.log(flight.return <= returnDate);
+        return (
+            (flight.from.includes(from) ||
+            flight.to.includes(to) ||
+            flight.return === returnDate||
+            flight.depart === depart) &&
+            flight.price <= price
+            
+        );
+    });
+    console.log("filtered flights: ", filteredFlights);
+    showTickets(filteredFlights);
+    
+});
 
 
