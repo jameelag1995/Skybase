d86bb010b88ba9b2bcfb9492b245c4f579bfa38e
jameelag1995 <agbariajameel@gmail.com>
2023-11-12 17:13:35 +0200

    fully fucntioal final

--- JS/homepage.js
+++ JS/homepage.js
@@ -1,64 +1,64 @@
 const flights = [
     {
-        id:0,
+        id: 0,
         from: "Tel aviv",
-        to:'amsterdam',
+        to: "amsterdam",
         price: 40,
-        dates:[
-            {depart: new Date ('11.24.2023')},
-            {return: new Date ('12.1.2023')}
-        ]
+        dates: [
+            { depart: new Date("11.24.2023") },
+            { return: new Date("12.1.2023") },
+        ],
     },
     {
-        id:1,
+        id: 1,
         from: "Tel aviv",
-        to:'london',
+        to: "london",
         price: 75,
-        dates:[
-            {depart: new Date ('11.28.2023')},
-            {return: new Date ('12.12.2023')}
-        ]
+        dates: [
+            { depart: new Date("11.28.2023") },
+            { return: new Date("12.12.2023") },
+        ],
     },
     {
-        id:2,
+        id: 2,
         from: "Athens",
-        to:'Prague',
+        to: "Prague",
         price: 95,
-        dates:[
-            {depart: new Date ('11.28.2023')},
-            {return: new Date ('12.12.2023')}
-        ]
+        dates: [
+            { depart: new Date("11.28.2023") },
+            { return: new Date("12.12.2023") },
+        ],
     },
     {
-        id:3,
+        id: 3,
         from: "Berlin",
-        to:'Prague',
+        to: "Prague",
         price: 22,
-        dates:[
-            {depart: new Date ('11.28.2023')},
-            {return: new Date ('12.12.2023')}
-        ]
+        dates: [
+            { depart: new Date("11.28.2023") },
+            { return: new Date("12.12.2023") },
+        ],
     },
     {
-        id:4,
+        id: 4,
         from: "London",
-        to:'Berlin',
+        to: "Berlin",
         price: 100,
-        dates:[
-            {depart: new Date ('11.28.2023')},
-            {return: new Date ('12.12.2023')}
-        ]
+        dates: [
+            { depart: new Date("11.28.2023") },
+            { return: new Date("12.12.2023") },
+        ],
     },
 ];
 
 const username = document.getElementById("username");
 const addFlight = document.getElementById("add-flight");
-if (localStorage.getItem('isAdmin') == 'true') {
+if (localStorage.getItem("isAdmin") == "true") {
     addFlight.style.display = "block";
-    username.innerText = `Admin ${localStorage.getItem('email')}`;
+    username.innerText = `Admin ${localStorage.getItem("email")}`;
 } else {
     addFlight.style.display = "none";
-    username.innerText = `${localStorage.getItem('email')}`;
+    username.innerText = `${localStorage.getItem("email")}`;
 }
 
 const flightsBoard = document.getElementById("flights-board");
@@ -66,7 +66,7 @@ showTickets(flights);
 
 function showTickets(flightsArr) {
     flightsBoard.innerHTML = "";
-    if (localStorage.getItem('isAdmin') == 'true') {
+    if (localStorage.getItem("isAdmin") == "true") {
         flightsArr.forEach((flight) => {
             let ticket = document.createElement("div");
             ticket.innerHTML = `<div>${flight.id}</div><div id="from-to">${
@@ -74,14 +74,16 @@ function showTickets(flightsArr) {
             } &#8594; ${flight.to}</div>
             <div id="departure-return-dates">${flight.dates[0].depart.toDateString()} &#8594; ${flight.dates[1].return.toDateString()}</div>
             <div id="price"><label for="priceinput">Price:</label>
-            <input type="number" id="priceinput" name="priceinput" value="${flight.price}" required>$</div><div id="btns"> <button class="btn add-to-cart">Add to Cart</button><button class="btn edit-price">Edit Price</button></div>`;
+            <input type="number" id="priceinput" name="priceinput" value="${
+                flight.price
+            }" required>$</div><div id="btns"> <button class="btn add-to-cart">Add to Cart</button><button class="btn edit-price">Edit Price</button></div>`;
             ticket.classList.add("ticket");
             flightsBoard.append(ticket);
         });
     } else {
         flightsArr.forEach((flight) => {
             let ticket = document.createElement("div");
-            ticket.innerHTML = `<div id="from-to">${flight.from} &#8594; ${
+            ticket.innerHTML = `<div>${flight.id}</div><div id="from-to">${flight.from} &#8594; ${
                 flight.to
             }</div>
             <div id="departure-return-dates">${flight.dates[0].depart.toDateString()} &#8594; ${flight.dates[1].return.toDateString()}</div>
@@ -139,7 +141,7 @@ searchFlight.addEventListener("submit", (e) => {
             (flight.from.includes(from) ||
                 flight.to.includes(to) ||
                 flight.return === returnDate ||
-                flight.depart === depart) &&
+                flight.depart === depart) ||
             flight.price <= price
         );
     });
@@ -167,22 +169,59 @@ editPriceBtns.forEach((editBtn) => {
         let id = ticket.firstElementChild.innerText;
         console.log(id);
         let currTicket = flights[id];
-        let newPrice = document.getElementById('priceinput').value;
+        let newPrice = document.getElementById("priceinput").value;
         currTicket.price = newPrice;
         showTickets(flights);
-        console.log('ticket & ticket price:',currTicket,currTicket.price);
-        
+        console.log("ticket & ticket price:", currTicket, currTicket.price);
     });
 });
-const cartBoard = document.getElementById('cart-board');
+const cartArr = [];
+const cartBoard = document.getElementById("cart-board");
 const addToCartBtns = document.querySelectorAll(".add-to-cart");
 addToCartBtns.forEach((CartBtn) => {
     CartBtn.addEventListener("click", (e) => {
         let ticket = CartBtn.parentElement.parentElement;
         console.log("ticket", ticket.innerHTML);
         let id = ticket.firstElementChild.innerText;
-        let currTicket = flights[id];
-        
-        
+
+        if (cartArr.includes(flights[id])) {
+        } else {
+            cartArr.push(flights[id]);
+        }
+        console.log('CartArr',cartArr);
+        showCart(cartArr);
     });
+});
+function showCart(cartlist) {
+    cartBoard.innerHTML = "";
+    cartlist.forEach((cartItem) => {
+        let item = document.createElement("div");
+        item.innerHTML = `<div id="from-to">${cartItem.from} &#8594; ${cartItem.to}</div>
+        <div id="departure-return-dates">${cartItem.dates[0].depart.toDateString()} &#8594; ${cartItem.dates[1].return.toDateString()}</div>
+        <div id="price"><b>${
+            cartItem.price
+        }$</b></div><div id=num-of-tickets><label for="ticketnum">Number of Tickets:</label>
+        <input type="number" id="ticketnum" name="ticketnum" value="1" required></div>`;
+        item.classList.add("ticket");
+        cartBoard.append(item);
+    });
+}
+
+
+const calcBtn = document.getElementById("calculate-total-btn");
+const totalEl = document.getElementById("total");
+let total = 0;
+calcBtn.addEventListener("click", (e) => {
+    const numOfTicketsInputs = document.querySelectorAll("#ticketnum");
+    cartArr.forEach((cartItem, index) => {
+        total += cartItem.price * numOfTicketsInputs[index].value;
+    });
+    totalEl.innerText = `${total}$`;
+    total = 0;
+});
+
+const logoutBtn = document.getElementById('logout-btn');
+logoutBtn.addEventListener('click',e =>{
+    window.location.href = '../HTML/Login.html';
+
 });
